6b3daf41cf513a34e4ff80775092239744427915
Daniel González Briseño <qbixmex@gmail.com>
2022-12-28 02:15:01 -0600

    adding-props-to-sidebar

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,21 +1,27 @@
 import { FC, ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
 import { boxStyles, avatarStyles } from './profile.styles';
+import PropTypes from 'prop-types';
 
-export const Profile: FC = (): ReactElement => {
+export const Profile: FC = (props: any): ReactElement => {
+  const { name = 'John'} = props;
   return (
     <Box sx={boxStyles}>
       <Avatar sx={avatarStyles}>
         <Typography variant='h4' color='text.primary'>
-          J
+          { `${name.substring(0, 1)}` }
         </Typography>
       </Avatar>
       <Typography variant='h6' color='text.primary'>
-        Welcome, John
+        Welcome, { name }
       </Typography>
       <Typography variant='body1' color='text.primary'>
         This is your personal task manager
       </Typography>
     </Box>
   );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string,
 };
