781cd4cd56fd135d9a33ce31ad26e66f624ad5e5
Daniel González Briseño <qbixmex@gmail.com>
2022-12-28 05:34:13 -0600

    counter-component-dynamic-props

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -5,15 +5,22 @@ import {
   avatarStyles,
   subtitleStyles,
 } from './taskCounter.styles';
+import { ITaskCounter } from './interfaces/ITaskCounter';
+import { Status } from '../createTaskForm/enums/Status';
 
-export const TaskCounter: FC = (): ReactElement => {
+export const TaskCounter: FC<ITaskCounter> = ({
+  status = Status.completed,
+  count = 0,
+}): ReactElement => {
   return (
     <Box sx={taskCounterStyles}>
       <Avatar sx={avatarStyles}>
-        <Typography color="white" variant="h4">10</Typography>
+        <Typography color="white" variant="h4">
+          {count}
+        </Typography>
       </Avatar>
       <Typography sx={subtitleStyles} variant="h5">
-        Subtitle
+        {status}
       </Typography>
     </Box>
   );
