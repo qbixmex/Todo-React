93939cc4617c3ca7b471dcec88ed261652190430
Daniel González Briseño <qbixmex@gmail.com>
2022-12-28 14:25:32 -0600

    styles-fixed-and-layout-reordered

--- src/pages/dashboard/dashboard.styles.ts
+++ src/pages/dashboard/dashboard.styles.ts
@@ -1,8 +1,5 @@
 export const sidebarStyles = {
   height: '100vh',
-  position: 'fixed',
-  right: 0,
-  top: 0,
   width: '100%',
   backgroundColor: 'background.paper',
   display: 'flex',
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -5,8 +5,8 @@ import { Sidebar, TaskArea } from '../../components';
 export const Dashboard: FC = (): ReactElement => {
   return (
     <Grid container minHeight="100vh" p={0} m={0}>
-      <TaskArea />
       <Sidebar />
+      <TaskArea />
     </Grid>
   );
 };
