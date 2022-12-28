bfb0f4a03213bb7a571b929a1d637753422777c7
Daniel González Briseño <qbixmex@gmail.com>
2022-12-28 02:06:18 -0600

    profile-component-layout

---
+++ src/components/profile/profile.styles.ts
@@ -0,0 +1,13 @@
+export const boxStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+export const avatarStyles = {
+  width: '96px',
+  height: '96px',
+  backgroundColor: 'primary.main',
+  marginBottom: '16px',
+};
--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,8 +1,21 @@
 import { FC, ReactElement } from 'react';
 import { Avatar, Box, Typography } from '@mui/material';
+import { boxStyles, avatarStyles } from './profile.styles';
 
 export const Profile: FC = (): ReactElement => {
   return (
-    <Avatar>A</Avatar>
+    <Box sx={boxStyles}>
+      <Avatar sx={avatarStyles}>
+        <Typography variant='h4' color='text.primary'>
+          J
+        </Typography>
+      </Avatar>
+      <Typography variant='h6' color='text.primary'>
+        Welcome, John
+      </Typography>
+      <Typography variant='body1' color='text.primary'>
+        This is your personal task manager
+      </Typography>
+    </Box>
   );
 };
