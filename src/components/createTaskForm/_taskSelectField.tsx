4d99ca484633b9aef0accd995ecaa8f66f8b6381
Daniel González Briseño <qbixmex@gmail.com>
2022-12-28 04:33:43 -0600

    task-select-field-interface-props

--- src/components/createTaskForm/_taskSelectField.tsx
+++ src/components/createTaskForm/_taskSelectField.tsx
@@ -3,21 +3,36 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { ISelectField } from './interfaces/ISelectField';
+import { SelectChangeEvent } from '@mui/material';
 
-export const TaskSelectField: FC = (): ReactElement => {
+export const TaskSelectField: FC<ISelectField> = ({
+  name = 'Select Box',
+  label = 'select box',
+  value = '',
+  onChange = (e: SelectChangeEvent) => console.log(e),
+  items = [{ value: '', label: 'Item' }],
+  disabled = false,
+}): ReactElement => {
   return (
     <FormControl fullWidth size="small">
-      <InputLabel id="status">Status</InputLabel>
+      <InputLabel id={`${name}-id`}>{label}</InputLabel>
       <Select
         labelId="status"
-        id="status-select"
-        value=""
+        id={`${name}-id-select`}
         label="Status"
-        name="status"
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
       >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {
+          items.map((item, index) => (
+            <MenuItem key={`${item.label}-${index + 1}`} value={item.value}>
+              {item.label}
+            </MenuItem>
+          ))
+        }
       </Select>
     </FormControl>
   );
