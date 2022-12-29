3edd8b34d5c4284970ff50d173265394dc68856c
Daniel González Briseño <qbixmex@gmail.com>
2022-12-29 09:54:50 -0600

    fetch-api

---
+++ src/helpers/sendApiRequest.ts
@@ -0,0 +1,40 @@
+type Method = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+const returnCorrectRequest = (
+  method: Method,
+  data: unknown
+): RequestInit => {
+  if (method === 'GET') {
+    return {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+  return {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+};
+
+export const sendApiRequest = async <T>(
+  url: string,
+  method: Method,
+  data: unknown = {},
+): Promise<T> => {
+  const response = await fetch(
+    url,
+    returnCorrectRequest(method, data),
+  );
+  if (!response.ok) {
+    const message = `An error has occurred: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return (await response.json()) as Promise<T>;
+};
