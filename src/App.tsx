3385c8a615beca1af275ea84c42e79c4f2d9d58b
Daniel González Briseño <qbixmex@gmail.com>
2022-12-29 09:34:06 -0600

    react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { FC, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { customTheme } from './theme/custom-theme';
 import { Dashboard } from './pages';
 
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
