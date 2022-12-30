126a1aa4ce344838a076a02480d2acc39a9f4f6f
Daniel González Briseño <qbixmex@gmail.com>
2022-12-30 09:24:57 -0600

    using-context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { FC, ReactElement } from 'react';
+import ComposeContext from './context/Compose.context';
+import { rootContext } from './context/root.context';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
@@ -10,10 +12,12 @@ const queryClient = new QueryClient();
 const App: FC = (): ReactElement => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={customTheme}>
-        <CssBaseline />
-        <Dashboard />
-      </ThemeProvider>
+      <ComposeContext components={rootContext}>
+        <ThemeProvider theme={customTheme}>
+          <CssBaseline />
+          <Dashboard />
+        </ThemeProvider>
+      </ComposeContext>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
